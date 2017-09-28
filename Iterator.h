d29cd2b552c41243e5516c72d9aade62ba285306
elliomic <elliomic@onid.orst.edu>
2017-09-27 23:30:51 -0700

    Iterators now store initial state. Changed the transform function to a List of functions to be applied in order. Added listTransform function that applies a list of functions to a given element. Added a restart function that returns the iterator to its initial state. Updated style.

--- Iterator.h
+++ Iterator.h
@@ -1,7 +1,8 @@
 #ifndef ITERATOR_H
 #define ITERATOR_H
 
-#include <stdlib.h>
+#include "LinkedList.h"
+#include "ListUtility.h"
 
 
 int inc(int x)
@@ -15,61 +16,88 @@ int dec(int x)
 }
 
 
+#define trans_fun_type(T) trans_fun_type##T
+#define trans_fun_typedef(T) typedef T (*trans_fun_type(T))(T);
+
+
 #define Iterator(T) Iterator##T
 #define IteratorDefinition(T) \
 typedef struct Iterator(T) { \
+	T start; \
 	T current; \
 	T (*gen)(T); \
-	T (*trans)(T); \
+	List(trans_fun_type##T) *trans; \
 } Iterator(T);
 
 
-#define identity(T) identity##T
-#define identityDefinition(T) \
-T identity(T)(T e)
+#define newIterator(T) newIterator##T
+#define newIteratorDefinition(T) \
+Iterator(T) *newIterator(T)(T start, T(*gen)(T)) \
 { \
-	return e; \
-}
-
-
-#define constructIterator(T) constructIterator##T
-#define constructIteratorDefinition(T) \
-Iterator(T) * constructIterator(T)(T start, T(*gen)(T)) \
-{ \
-	Iterator(T) * iter = malloc(sizeof(Iterator(T))); \
-	iter->current = start; \
+	Iterator(T) *iter = malloc(sizeof(Iterator(T))); \
+	iter->start = iter->current = start; \
 	iter->gen = gen; \
-	iter->trans = *identity(T); \
+	iter->trans = newList(trans_fun_type##T)(); \
 	return iter; \
 }
 
+#define listTransform(T) listTransform##T
+#define listTransformDefinition(T) \
+T listTransform(T)(List(trans_fun_type##T) *trans, T e) \
+{ \
+	if (listSize(trans_fun_type##T)(trans) == 0) { \
+		return e; \
+	} \
+\
+	List(trans_fun_type##T) *tail = tailList(trans_fun_type##T)(trans); \
+	T ret = listTransform(T)(tail, \
+	                         headList(trans_fun_type##T)(trans)(e)); \
+	free(tail); \
+	return ret; \
+}
 
 #define currentIter(T) currentIter##T
 #define currentIterDefinition(T) \
-T currentIter(T)(Iterator(T) * iter) \
+T currentIter(T)(Iterator(T) *iter) \
 { \
-	return iter->trans(iter->current); \
+	return listTransform(T)(iter->trans, iter->current); \
 }
-
 
 #define nextIter(T) nextIter##T
 #define nextIterDefinition(T) \
-T nextIter(T)(Iterator(T) * iter) \
+T nextIter(T)(Iterator(T) *iter) \
 { \
 	iter->current = iter->gen(iter->current); \
-	return iter->trans(iter->current); \
+	return listTransform(T)(iter->trans, iter->current); \
+}
+
+#define restartIter(T) restartIter##T
+#define restartIterDefinition(T) \
+T restartIter(T)(Iterator(T) *iter) \
+{ \
+	iter->current = iter->start; \
+	return listTransform(T)(iter->trans, iter->current); \
 }
 
 
 #define IteratorLib(T) \
+trans_fun_typedef(T) \
+\
+LinkedListLib(trans_fun_type(T)) \
+\
+ListUtilityLib(trans_fun_type(T)) \
+\
 IteratorDefinition(T) \
 \
-identityDefinition(T) \
+newIteratorDefinition(T) \
 \
-constructIteratorDefinition(T)	\
+listTransformDefinition(T) \
 \
 currentIterDefinition(T) \
 \
 nextIterDefinition(T) \
+\
+restartIterDefinition(T) \
+
 
 #endif
